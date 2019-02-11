fcc60bdaa77225119fe54d1e0674419be34bb421
Amit Bothra <amit.bothra@outlook.com>
2019-02-11 06:49:40 -0600

    multiple inputs single datapoint neuralnetwork eg

---
+++ Code/chapter03.py
@@ -0,0 +1,39 @@
+
+# One input neural network
+# def neural_network(input, weight):
+#   prediction = input * weight
+#   return prediction
+
+# weight = 0.1
+# number_of_toes = [8.5,9.5,10,9]
+# input = number_of_toes[0]
+
+# pred = neural_network(input,weight)
+# print(pred)
+
+# Multi Input Neural Network
+
+def w_sum(a,b):
+  assert(len(a) == len(b))
+  output = 0
+  for i in range(len(a)):
+    output += (a[i] * b[i])
+  return output
+
+def neural_network(input,weights):
+  prediction = w_sum(input,weights)
+  return prediction
+
+#inputs
+toes = [8.5,9.5,9.9,9.0]
+wlrec = [0.65,0.8,0.8,0.9]
+nfans = [1.2,1.3,0.5,1.0]
+
+input = [toes[0], wlrec[0], nfans[0]]
+weights = [0.1, 0.2, 0]
+
+pred = neural_network(input,weights)
+print(pred)
+
+
+  
