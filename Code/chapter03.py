d0e0219fd4bb44dbdfa4f3929196e928ad2993e8
Amit Bothra <amit.bothra@outlook.com>
2019-02-11 06:59:24 -0600

    numpy version multi input single datapoint neural network

--- Code/chapter03.py
+++ Code/chapter03.py
@@ -11,29 +11,46 @@
 # pred = neural_network(input,weight)
 # print(pred)
 
-# Multi Input Neural Network
+# # Multi Input Neural Network
 
-def w_sum(a,b):
-  assert(len(a) == len(b))
-  output = 0
-  for i in range(len(a)):
-    output += (a[i] * b[i])
-  return output
+# def w_sum(a,b):
+#   assert(len(a) == len(b))
+#   output = 0
+#   for i in range(len(a)):
+#     output += (a[i] * b[i])
+#   return output
 
-def neural_network(input,weights):
-  prediction = w_sum(input,weights)
+# def neural_network(input,weights):
+#   prediction = w_sum(input,weights)
+#   return prediction
+
+# #inputs
+# toes = [8.5,9.5,9.9,9.0]
+# wlrec = [0.65,0.8,0.8,0.9]
+# nfans = [1.2,1.3,0.5,1.0]
+
+# input = [toes[0], wlrec[0], nfans[0]]
+# weights = [0.1, 0.2, 0]
+
+# pred = neural_network(input,weights)
+# print(pred)
+
+# Multi-input Numpy Version neural network
+import numpy as np
+
+def neural_network(input, weights):
+  prediction = input.dot(weights)
   return prediction
 
 #inputs
-toes = [8.5,9.5,9.9,9.0]
-wlrec = [0.65,0.8,0.8,0.9]
-nfans = [1.2,1.3,0.5,1.0]
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
-input = [toes[0], wlrec[0], nfans[0]]
-weights = [0.1, 0.2, 0]
+input = np.array([toes[0], wlrec[0], nfans[0]])
+weights = np.array([0.1, 0.2, 0])
 
 pred = neural_network(input,weights)
 print(pred)
-
 
   
