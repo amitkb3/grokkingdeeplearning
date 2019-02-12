9356de4cf926338212b55a925d7d1944fe72215d
Amit Bothra <amit.bothra@outlook.com>
2019-02-12 07:05:28 -0600

    single datapoint multiple output neural network

--- Code/chapter03.py
+++ Code/chapter03.py
@@ -35,22 +35,42 @@
 # pred = neural_network(input,weights)
 # print(pred)
 
-# Multi-input Numpy Version neural network
-import numpy as np
+# # Multi-input Numpy Version neural network
+# import numpy as np
+
+# def neural_network(input, weights):
+#   prediction = input.dot(weights)
+#   return prediction
+
+# #inputs
+# toes = np.array([8.5, 9.5, 9.9, 9.0])
+# wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+# nfans = np.array([1.2, 1.3, 0.5, 1.0])
+
+# input = np.array([toes[0], wlrec[0], nfans[0]])
+# weights = np.array([0.1, 0.2, 0])
+
+# pred = neural_network(input,weights)
+# print(pred)
+
+#Single input multiple output neural network
+def ele_mul(number, vector):
+  output = [0,0,0]
+  assert(len(output) == len(vector))
+
+  for i in range(len(vector)):
+    output[i] = number * vector[i]
+  
+  return output
 
 def neural_network(input, weights):
-  prediction = input.dot(weights)
+  prediction = ele_mul(input,weights)
   return prediction
 
-#inputs
-toes = np.array([8.5, 9.5, 9.9, 9.0])
-wlrec = np.array([0.65, 0.8, 0.8, 0.9])
-nfans = np.array([1.2, 1.3, 0.5, 1.0])
-
-input = np.array([toes[0], wlrec[0], nfans[0]])
-weights = np.array([0.1, 0.2, 0])
+#input
+wlrec = [0.65, 0.8, 0.8, 0.9]
+input = wlrec[0]
+weights = [0.3, 0.2, 0.9]
 
 pred = neural_network(input,weights)
-print(pred)
-
-  
+print(pred)  
