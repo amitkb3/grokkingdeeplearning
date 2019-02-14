0eafee02aff52dd45409f52ab2a312a0c62d05f2
Amit Bothra <amit.bothra@outlook.com>
2019-02-13 22:19:41 -0600

    multi-input, multi-output neural network

--- Code/chapter03.py
+++ Code/chapter03.py
@@ -54,23 +54,61 @@
 # print(pred)
 
 #Single input multiple output neural network
-def ele_mul(number, vector):
-  output = [0,0,0]
-  assert(len(output) == len(vector))
+# def ele_mul(number, vector):
+#   output = [0,0,0]
+#   assert(len(output) == len(vector))
 
-  for i in range(len(vector)):
-    output[i] = number * vector[i]
+#   for i in range(len(vector)):
+#     output[i] = number * vector[i]
+  
+#   return output
+
+# def neural_network(input, weights):
+#   prediction = ele_mul(input,weights)
+#   return prediction
+
+# #input
+# wlrec = [0.65, 0.8, 0.8, 0.9]
+# input = wlrec[0]
+# weights = [0.3, 0.2, 0.9]
+
+# pred = neural_network(input,weights)
+# print(pred)  
+
+#Multi-input Multi-output Neural Network
+def w_sum(a,b):
+  assert(len(a) == len(b))
+  output = 0
+  for i in range(len(a)):
+    output += a[i] * b[i]
+  return output
+
+def vect_mat_mult(vect, matrix):
+  assert(len(vect) == len(matrix))
+  output = [0, 0, 0]
+
+  for i in range(len(vect)):
+    output[i] = w_sum(vect, matrix[i])
   
   return output
 
 def neural_network(input, weights):
-  prediction = ele_mul(input,weights)
+  prediction = vect_mat_mult(input,weights)
   return prediction
 
-#input
+#inputs
+toes = [8.5, 9.5, 9.9, 9.0]
 wlrec = [0.65, 0.8, 0.8, 0.9]
-input = wlrec[0]
-weights = [0.3, 0.2, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+input = [toes[0], wlrec[0], nfans[0]]
+
+weights = [[0.1, 0.1, -0.3],
+           [0.1, 0.2, 0.0],
+           [0.0, 1.3, 0.1] ]
 
 pred = neural_network(input,weights)
-print(pred)  
+print(pred)
+
+
+
