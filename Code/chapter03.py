8871774b28aa1acafd4fb941e60d46d04a77f253
Amit Bothra <amit.bothra@outlook.com>
2019-02-14 07:14:43 -0600

    numpy primer

--- Code/chapter03.py
+++ Code/chapter03.py
@@ -153,35 +153,92 @@
 # print(pred)
 
 #Numpy version - Stacked Neural Network
+# import numpy as np
+
+# def neural_network(input, weights):
+#   hid = input.dot(weights[0])
+#   prediction = hid.dot(weights[1])
+#   return prediction
+
+# #toes #% win # fans
+# ih_wgt = np.array([
+#           [0.1, 0.2, -0.1], #hid[0]
+#           [-0.1, 0.1, 0.9], #hid[1]
+#           [0.1, 0.4, 0.1]]) #hid[2]
+
+# hp_wgt = np.array([
+#           [0.3, 1.1, -0.3], # hurt?
+#           [0.1, 0.2, 0.0], # win ?
+#           [0.0, 1.3, 0.1]]) # sad ?
+
+# weights = [ih_wgt, hp_wgt]
+
+# #inputs
+# toes = np.array([8.5, 9.5, 9.9, 9.0])
+# wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+# nfans = np.array([1.2, 1.3, 0.5, 1.0])
+
+# input = np.array([toes[0], wlrec[0], nfans[0]])
+
+# pred = neural_network(input,weights)
+# print(pred)
+
+#Basic primer on Numpy
 import numpy as np
 
-def neural_network(input, weights):
-  hid = input.dot(weights[0])
-  prediction = hid.dot(weights[1])
-  return prediction
+# a = np.array([0,1,2,3])
+# b = np.array([4,5,6,7])
+# c= np.array([[0,1,2,3],
+#              [4,5,6,7]])
+# d = np.zeros((2,4))
+# e = np.random.rand(2,5)
 
-#toes #% win # fans
-ih_wgt = np.array([
-          [0.1, 0.2, -0.1], #hid[0]
-          [-0.1, 0.1, 0.9], #hid[1]
-          [0.1, 0.4, 0.1]]) #hid[2]
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
 
-hp_wgt = np.array([
-          [0.3, 1.1, -0.3], # hurt?
-          [0.1, 0.2, 0.0], # win ?
-          [0.0, 1.3, 0.1]]) # sad ?
+# print(a * 0.1)
+# print(c * 0.2)
+# print(a * b)
+# print(a * b * 0.2)
+# print(a * c)
+# print( a * e)
 
-weights = [ih_wgt, hp_wgt]
+# a = np.zeros((1,4))
+# b = np.zeros((4,3))
 
-#inputs
-toes = np.array([8.5, 9.5, 9.9, 9.0])
-wlrec = np.array([0.65, 0.8, 0.8, 0.9])
-nfans = np.array([1.2, 1.3, 0.5, 1.0])
+# c = a.dot(b)
+# print(c.shape)
 
-input = np.array([toes[0], wlrec[0], nfans[0]])
+a = np.zeros((2,4))
+b = np.zeros((4,3))
+c = a.dot(b)
+print(c.shape)
 
-pred = neural_network(input,weights)
-print(pred)
+e = np.zeros((2,1))
+f = np.zeros((1,3))
+
+g = e.dot(f)
+print(g.shape)
+
+h = np.zeros((5,4)).T
+i = np.zeros((5,6))
+
+j = h.dot(i)
+print(j.shape)
+
+h = np.zeros((5,4))
+i = np.zeros((5,6))
+
+j = h.dot(i)
+print(j.shape)
+
+
+
+
+
 
 
 
