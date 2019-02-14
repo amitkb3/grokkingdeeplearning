e53494e927fb8531b9809aa91abba27e17740913
Amit Bothra <amit.bothra@outlook.com>
2019-02-13 22:36:33 -0600

    stacked neural network

--- Code/chapter03.py
+++ Code/chapter03.py
@@ -75,7 +75,42 @@
 # pred = neural_network(input,weights)
 # print(pred)  
 
-#Multi-input Multi-output Neural Network
+# #Multi-input Multi-output Neural Network
+# def w_sum(a,b):
+#   assert(len(a) == len(b))
+#   output = 0
+#   for i in range(len(a)):
+#     output += a[i] * b[i]
+#   return output
+
+# def vect_mat_mult(vect, matrix):
+#   assert(len(vect) == len(matrix))
+#   output = [0, 0, 0]
+
+#   for i in range(len(vect)):
+#     output[i] = w_sum(vect, matrix[i])
+  
+#   return output
+
+# def neural_network(input, weights):
+#   prediction = vect_mat_mult(input,weights)
+#   return prediction
+
+# #inputs
+# toes = [8.5, 9.5, 9.9, 9.0]
+# wlrec = [0.65, 0.8, 0.8, 0.9]
+# nfans = [1.2, 1.3, 0.5, 1.0]
+
+# input = [toes[0], wlrec[0], nfans[0]]
+
+# weights = [[0.1, 0.1, -0.3],
+#            [0.1, 0.2, 0.0],
+#            [0.0, 1.3, 0.1] ]
+
+# pred = neural_network(input,weights)
+# print(pred)
+
+#Stacked neural network
 def w_sum(a,b):
   assert(len(a) == len(b))
   output = 0
@@ -93,7 +128,8 @@ def vect_mat_mult(vect, matrix):
   return output
 
 def neural_network(input, weights):
-  prediction = vect_mat_mult(input,weights)
+  hid = vect_mat_mult(input, weights[0])
+  prediction = vect_mat_mult(hid,weights[1])
   return prediction
 
 #inputs
@@ -103,12 +139,15 @@ nfans = [1.2, 1.3, 0.5, 1.0]
 
 input = [toes[0], wlrec[0], nfans[0]]
 
-weights = [[0.1, 0.1, -0.3],
+ih_wgt  = [[0.1, 0.2, -0.1],
+           [-0.1, 0.1, 0.9],
+           [0.1, 0.4, 0.1] ]
+
+hp_wgt  = [[0.3, 1.1, -0.3],
            [0.1, 0.2, 0.0],
            [0.0, 1.3, 0.1] ]
 
+weights = [ih_wgt, hp_wgt]
+
 pred = neural_network(input,weights)
 print(pred)
-
-
-
