89c8751b9fe065e04af461ccb16fb1383c586538
Amit Bothra <amit.bothra@outlook.com>
2019-02-14 06:53:01 -0600

    numpy - stacked neural network

--- Code/chapter03.py
+++ Code/chapter03.py
@@ -110,44 +110,78 @@
 # pred = neural_network(input,weights)
 # print(pred)
 
-#Stacked neural network
-def w_sum(a,b):
-  assert(len(a) == len(b))
-  output = 0
-  for i in range(len(a)):
-    output += a[i] * b[i]
-  return output
+# #Stacked neural network
+# def w_sum(a,b):
+#   assert(len(a) == len(b))
+#   output = 0
+#   for i in range(len(a)):
+#     output += a[i] * b[i]
+#   return output
 
-def vect_mat_mult(vect, matrix):
-  assert(len(vect) == len(matrix))
-  output = [0, 0, 0]
+# def vect_mat_mult(vect, matrix):
+#   assert(len(vect) == len(matrix))
+#   output = [0, 0, 0]
 
-  for i in range(len(vect)):
-    output[i] = w_sum(vect, matrix[i])
+#   for i in range(len(vect)):
+#     output[i] = w_sum(vect, matrix[i])
   
-  return output
+#   return output
+
+# def neural_network(input, weights):
+#   hid = vect_mat_mult(input, weights[0])
+#   prediction = vect_mat_mult(hid,weights[1])
+#   return prediction
+
+# #inputs
+# toes = [8.5, 9.5, 9.9, 9.0]
+# wlrec = [0.65, 0.8, 0.8, 0.9]
+# nfans = [1.2, 1.3, 0.5, 1.0]
+
+# input = [toes[0], wlrec[0], nfans[0]]
+
+# ih_wgt  = [[0.1, 0.2, -0.1],
+#            [-0.1, 0.1, 0.9],
+#            [0.1, 0.4, 0.1] ]
+
+# hp_wgt  = [[0.3, 1.1, -0.3],
+#            [0.1, 0.2, 0.0],
+#            [0.0, 1.3, 0.1] ]
+
+# weights = [ih_wgt, hp_wgt]
+
+# pred = neural_network(input,weights)
+# print(pred)
+
+#Numpy version - Stacked Neural Network
+import numpy as np
 
 def neural_network(input, weights):
-  hid = vect_mat_mult(input, weights[0])
-  prediction = vect_mat_mult(hid,weights[1])
+  hid = input.dot(weights[0])
+  prediction = hid.dot(weights[1])
   return prediction
 
-#inputs
-toes = [8.5, 9.5, 9.9, 9.0]
-wlrec = [0.65, 0.8, 0.8, 0.9]
-nfans = [1.2, 1.3, 0.5, 1.0]
+#toes #% win # fans
+ih_wgt = np.array([
+          [0.1, 0.2, -0.1], #hid[0]
+          [-0.1, 0.1, 0.9], #hid[1]
+          [0.1, 0.4, 0.1]]) #hid[2]
 
-input = [toes[0], wlrec[0], nfans[0]]
-
-ih_wgt  = [[0.1, 0.2, -0.1],
-           [-0.1, 0.1, 0.9],
-           [0.1, 0.4, 0.1] ]
-
-hp_wgt  = [[0.3, 1.1, -0.3],
-           [0.1, 0.2, 0.0],
-           [0.0, 1.3, 0.1] ]
+hp_wgt = np.array([
+          [0.3, 1.1, -0.3], # hurt?
+          [0.1, 0.2, 0.0], # win ?
+          [0.0, 1.3, 0.1]]) # sad ?
 
 weights = [ih_wgt, hp_wgt]
 
+#inputs
+toes = np.array([8.5, 9.5, 9.9, 9.0])
+wlrec = np.array([0.65, 0.8, 0.8, 0.9])
+nfans = np.array([1.2, 1.3, 0.5, 1.0])
+
+input = np.array([toes[0], wlrec[0], nfans[0]])
+
 pred = neural_network(input,weights)
 print(pred)
+
+
+
