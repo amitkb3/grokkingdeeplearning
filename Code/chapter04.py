6b81066ad1534709d44f4b035753da1511591e32
Amit Bothra <amit.bothra@outlook.com>
2019-02-15 20:51:15 -0600

    multi-iteration hot&cold learning

--- Code/chapter04.py
+++ Code/chapter04.py
@@ -11,38 +11,66 @@
 # print(error)
 
 # Hot and Cold Learning
-def neural_network(input,weight):
-  prediction = input * weight
-  return prediction
+# def neural_network(input,weight):
+#   prediction = input * weight
+#   return prediction
 
-#input
-number_of_toes = [8.5]
-win_or_loss_binary = [1]
+# #input
+# number_of_toes = [8.5]
+# win_or_loss_binary = [1]
 
-input = number_of_toes[0]
-true = win_or_loss_binary[0]
-weight = 0.1
-lr = 0.01
+# input = number_of_toes[0]
+# true = win_or_loss_binary[0]
+# weight = 0.1
+# lr = 0.01
 
-pred = neural_network(input, weight)
+# pred = neural_network(input, weight)
 
-error = (pred - true) ** 2
-print(error)
+# error = (pred - true) ** 2
+# print(error)
 
-p_up = neural_network(input, weight + lr)
-e_up = (p_up - true) ** 2
-print(e_up)
+# p_up = neural_network(input, weight + lr)
+# e_up = (p_up - true) ** 2
+# print(e_up)
 
-p_dn = neural_network(input, weight -lr)
-e_dn = (p_dn - true) ** 2
-print(e_dn)
+# p_dn = neural_network(input, weight -lr)
+# e_dn = (p_dn - true) ** 2
+# print(e_dn)
 
-if(error > e_dn or  error > e_up):
-  if(e_dn < e_up):
-    weight -= lr
-  if(e_up < e_dn):
-    weight += lr
-print(weight)
+# if(error > e_dn or  error > e_up):
+#   if(e_dn < e_up):
+#     weight -= lr
+#   if(e_up < e_dn):
+#     weight += lr
+# print(weight)
+
+# Multi-iteration Hot&Cold Learning
+weight = 0.5
+input = 0.5
+goal_prediction = 0.8
+
+step_amount =  0.001
+
+for iteration in range(1101):
+  prediction = input*weight
+  error = (prediction - goal_prediction) ** 2
+
+  print(f"Error: {error} Prediction: {prediction}")
+
+  up_prediction = input * (weight + step_amount)
+  up_error = (up_prediction - goal_prediction) ** 2
+
+  down_prediction = input * (weight - step_amount)
+  down_error = (down_prediction - goal_prediction) ** 2
+
+  if(down_error < up_error):
+    weight = weight - step_amount
+  
+  if(down_error > up_error):
+    weight = weight + step_amount
+
+
+
 
 
 
