5c48e2d17f584652aca64c8e8cab9f4fc5653eca
Amit Bothra <amit.bothra@outlook.com>
2019-02-27 21:55:27 -0600

    updated chapter04

--- Code/chapter04.py
+++ Code/chapter04.py
@@ -1,14 +1,14 @@
-# Measuring Error
+Measuring Error
 
-# knob_weight = 0.5
-# input = 0.5
-# goal_pred = 0.8
+knob_weight = 0.5
+input = 0.5
+goal_pred = 0.8
 
-# pred = input * knob_weight
+pred = input * knob_weight
 
-# error = (pred - goal_pred) ** 2
+error = (pred - goal_pred) ** 2
 
-# print(error)
+print(error)
 
 # Hot and Cold Learning
 # def neural_network(input,weight):
